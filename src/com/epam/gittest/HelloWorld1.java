5cfebd9802ca6df403848eb8eb427c06a049ad4c
DenisBorisov3321 <51997841+DenisBorisov3321@users.noreply.github.com>
2019-06-20 15:38:19 +0400

    master change

---
+++ src/com/epam/gittest/HelloWorld1.java
@@ -0,0 +1,4 @@
+package com.epam.gittest;
+
+public class HelloWorld1 {
+}
--- src/com/epam/gittest/HelloWorld4.java
+++
@@ -1,7 +0,0 @@
-package com.epam.gittest;
-
-public class HelloWorld4 {
-    public static void main(String[] args) {
-        System.out.println("Buenas dias, World!");
-    }
-}
