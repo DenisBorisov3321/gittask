380a515b12fca28cda921ec980b33f5eb771f37c
DenisBorisov3321 <51997841+DenisBorisov3321@users.noreply.github.com>
2019-06-21 16:45:51 +0400

    chenges in master branch for rebase

--- src/com/epam/gittest/Gittask.java
+++ src/com/epam/gittest/Gittask.java
@@ -3,6 +3,7 @@ package com.epam.gittest;
 public class Gittask {
     public static void main(String[] args) {
         System.out.println("Hi, Git!");        
+        System.out.println("I want rebase check");
         System.out.println("make rebase check");
     }
 }
