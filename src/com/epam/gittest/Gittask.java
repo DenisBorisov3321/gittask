7baff45c10a06c047d154fb29ac42b4056ec8471
Denis_Borisov <md5-82@mail.ru>
2019-06-21 11:37:36 +0400

    changes for pull conflicts test

--- src/com/epam/gittest/Gittask.java
+++ src/com/epam/gittest/Gittask.java
@@ -2,6 +2,6 @@ package com.epam.gittest;
 
 public class Gittask {
     public static void main(String[] args) {
-        System.out.println("Hello, Git!");
+        System.out.println("Hi, Git!");
     }
 }
