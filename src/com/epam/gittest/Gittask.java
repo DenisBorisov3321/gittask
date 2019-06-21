8cf187385afd154ddddfae6d0e0750318ba5d200
DenisBorisov3321 <51997841+DenisBorisov3321@users.noreply.github.com>
2019-06-21 15:14:13 +0400

    commit chenges for published rollback

--- src/com/epam/gittest/Gittask.java
+++ src/com/epam/gittest/Gittask.java
@@ -3,6 +3,5 @@ package com.epam.gittest;
 public class Gittask {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
-        System.out.println("Create branch1-1");
     }
 }
