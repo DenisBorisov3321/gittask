1676dbdd5705010f6cafe2a33b0e983658dc4fda
DenisBorisov3321 <51997841+DenisBorisov3321@users.noreply.github.com>
2019-06-21 14:17:48 +0400

    commit changes for rollback after commit

--- src/com/epam/gittest/Gittask.java
+++ src/com/epam/gittest/Gittask.java
@@ -2,6 +2,6 @@ package com.epam.gittest;
 
 public class Gittask {
     public static void main(String[] args) {
-        System.out.println("Hi, Git!");
+        System.out.println("changed - need to rollback to Hi, Git");
     }
 }
