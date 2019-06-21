6feb7fcc1f68ae7aa6bd507d4d69b106843acc15
Denis_Borisov <md5-82@mail.ru>
2019-06-21 11:50:54 +0400

    branch1-1 creat/update

--- src/com/epam/gittest/Gittask.java
+++ src/com/epam/gittest/Gittask.java
@@ -3,6 +3,6 @@ package com.epam.gittest;
 public class Gittask {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
-        System.out.println("Create branch1");
+        System.out.println("Create branch1-1");
     }
 }
