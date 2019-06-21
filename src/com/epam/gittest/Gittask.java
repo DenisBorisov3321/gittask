283703bea7dee3432846b99fa6fd9b33cb6ec601
DenisBorisov3321 <51997841+DenisBorisov3321@users.noreply.github.com>
2019-06-21 15:38:44 +0400

    Update Gittask.java

--- src/com/epam/gittest/Gittask.java
+++ src/com/epam/gittest/Gittask.java
@@ -4,5 +4,6 @@ public class Gittask {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         System.out.println("Create branch1-1");
+        System.out.println("Make pull request");
     }
 }
